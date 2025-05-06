f30aa7da846821562541d1daa1907ac1fd75a992
Tim Sinclair <Tim.Sinclair@live.com>
2025-05-06 09:30:26 -0700

    Trigger redeploy

--- startup.sh
+++ startup.sh
@@ -1,2 +1,13 @@
-- name: Make startup.sh executable
-  run: chmod +x startup.sh
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Change to the directory where Azure deploys the app
+cd /home/site/wwwroot
+
+# Ensure dependencies are installed
+npm install --production
+
+# Start the Astro SSR server
+npm start
