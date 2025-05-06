3a5720d790ce20d315dd889b405e02a6a3f23109
Tim Sinclair <Tim.Sinclair@live.com>
2025-05-06 12:36:58 -0700

    Expose server on 0.0.0.0 for Azure compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   output: 'server',
   adapter: nodeAdapter({ mode: 'standalone' }),
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 8080
+  },
   integrations: [tailwind()]
 });
 
